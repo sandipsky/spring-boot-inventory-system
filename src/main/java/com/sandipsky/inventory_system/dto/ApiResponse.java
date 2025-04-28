425bde68b7c6d4e10def173347422c5a418e25a9
sandipsky <sandipshakya75@gmail.com>
2025-04-28 14:28:08 +0545

    changes

--- src/main/java/com/sandipsky/inventory_system/dto/ApiResponse.java
+++ src/main/java/com/sandipsky/inventory_system/dto/ApiResponse.java
@@ -1,7 +1,5 @@
 package com.sandipsky.inventory_system.dto;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
@@ -14,7 +12,6 @@ public class ApiResponse<T> {
     private String message;
     @JsonProperty("post_data_id")
     private int data;
-    private List<String> errors;
     private int errorCode;
     private long timestamp;
 }
--- src/main/java/com/sandipsky/inventory_system/util/ResponseUtil.java
+++ src/main/java/com/sandipsky/inventory_system/util/ResponseUtil.java
@@ -1,8 +1,4 @@
 package com.sandipsky.inventory_system.util;
-
-import java.util.Arrays;
-import java.util.List;
-
 import com.sandipsky.inventory_system.dto.ApiResponse;
 
 public class ResponseUtil {
@@ -12,23 +8,17 @@ public class ResponseUtil {
         response.setSuccess(true);
         response.setMessage(message);
         response.setData(data);
-        response.setErrors(null);
         response.setErrorCode(0); // No error
         response.setTimestamp(System.currentTimeMillis());
         return response;
     }
 
-    public static <T> ApiResponse<T> error(List<String> errors, String message, int errorCode) {
+    public static <T> ApiResponse<T> error(String message, int errorCode) {
         ApiResponse<T> response = new ApiResponse<>();
         response.setSuccess(false);
         response.setMessage(message);
-        response.setErrors(errors);
         response.setErrorCode(errorCode);
         response.setTimestamp(System.currentTimeMillis());
         return response;
-    }
-
-    public static <T> ApiResponse<T> error(String error, String message, int errorCode) {
-        return error(Arrays.asList(error), message, errorCode);
     }
 }
