f552f46b81c15065a102d239a17f8a0ed0fe37e1
sandipsky <sandipshakya75@gmail.com>
2025-05-16 17:40:13 +0545

    changes

--- src/main/java/com/sandipsky/inventory_system/service/MasterPurchaseEntryService.java
+++ src/main/java/com/sandipsky/inventory_system/service/MasterPurchaseEntryService.java
@@ -2,23 +2,19 @@ package com.sandipsky.inventory_system.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sandipsky.inventory_system.dto.purchase.MasterPurchaseEntryDTO;
 import com.sandipsky.inventory_system.dto.purchase.PurchaseEntryDTO;
 import com.sandipsky.inventory_system.dto.ProductDTO;
 import com.sandipsky.inventory_system.dto.filter.RequestDTO;
-import com.sandipsky.inventory_system.entity.Category;
 import com.sandipsky.inventory_system.entity.MasterPurchaseEntry;
 import com.sandipsky.inventory_system.entity.Product;
 import com.sandipsky.inventory_system.entity.PurchaseEntry;
-import com.sandipsky.inventory_system.entity.Unit;
-import com.sandipsky.inventory_system.exception.DuplicateResourceException;
 import com.sandipsky.inventory_system.exception.ResourceNotFoundException;
-import com.sandipsky.inventory_system.repository.CategoryRepository;
 import com.sandipsky.inventory_system.repository.MasterPurchaseEntryRepository;
 import com.sandipsky.inventory_system.repository.PartyRepository;
-import com.sandipsky.inventory_system.repository.MasterPurchaseEntryRepository;
-import com.sandipsky.inventory_system.repository.UnitRepository;
+import com.sandipsky.inventory_system.repository.ProductRepository;
 import com.sandipsky.inventory_system.util.SpecificationBuilder;
 
 import org.springframework.data.domain.*;
@@ -35,9 +31,13 @@ public class MasterPurchaseEntryService {
     @Autowired
     private PartyRepository partyRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     private final SpecificationBuilder<MasterPurchaseEntry> specBuilder = new SpecificationBuilder<>();
 
-    public MasterPurchaseEntry saveMasterPurchaseEntry(MasterPurchaseEntryDTO dto) {
+    @Transactional
+    public MasterPurchaseEntry saveMasterPurchaseEntry(MasterPurchaseEntryDTO masterPurchaseEntryDTO) {
         // if (repository.existsByName(dto.getName())) {
         // throw new DuplicateResourceException("MasterPurchaseEntry with the same name
         // already exists");
@@ -48,9 +48,37 @@ public class MasterPurchaseEntryService {
         // blank");
         // }
 
-        MasterPurchaseEntry product = new MasterPurchaseEntry();
-        mapDtoToEntity(dto, product);
-        return repository.save(product);
+        // if (dto.getPartyId() != null) {
+        // entity.setParty(partyRepository.findById(dto.getPartyId())
+        // .orElseThrow(() -> new ResourceNotFoundException("Party not found")));
+        // }
+
+        MasterPurchaseEntry masterPurchaseEntry = new MasterPurchaseEntry();
+        mapDtoToEntity(masterPurchaseEntryDTO, masterPurchaseEntry);
+
+        if (masterPurchaseEntryDTO.getPurchaseEntries() != null) {
+            List<PurchaseEntry> purchaseEntries = masterPurchaseEntryDTO.getPurchaseEntries().stream().map(pedto -> {
+                PurchaseEntry purchaseEntry = new PurchaseEntry();
+                purchaseEntry.setBatch(pedto.getBatch());
+                purchaseEntry.setCostPrice(pedto.getCostPrice());
+                purchaseEntry.setSellingPrice(pedto.getSellingPrice());
+                purchaseEntry.setMrp(pedto.getMrp());
+
+                Product product = productRepository.findById(pedto.getProductId())
+                        .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+
+                product.setCostPrice(pedto.getCostPrice());
+                product.setSellingPrice(pedto.getSellingPrice());
+                product.setMrp(pedto.getMrp());
+                purchaseEntry.setProduct(product);
+                
+                productRepository.save(product);
+                return purchaseEntry;
+            }).toList();
+            masterPurchaseEntry.setPurchaseEntries(purchaseEntries);
+        }
+
+        return repository.save(masterPurchaseEntry);
     }
 
     public Page<MasterPurchaseEntryDTO> getPaginatedMasterPurchaseEntrysList(RequestDTO request) {
@@ -155,27 +183,5 @@ public class MasterPurchaseEntryService {
         entity.setGrandTotal(dto.getGrandTotal());
         entity.setDiscountType(dto.getDiscountType());
         entity.setRemarks(dto.getRemarks());
-
-        if (dto.getPurchaseEntries() != null) {
-            List<PurchaseEntry> purchaseEntries = dto.getPurchaseEntries().stream().map(pedto -> {
-                PurchaseEntry pe = new PurchaseEntry();
-                pe.setBatch(pedto.getBatch());
-                pe.setCostPrice(pedto.getCostPrice());
-                pe.setSellingPrice(pedto.getSellingPrice());
-                pe.setMrp(pedto.getMrp());
-                // if (pedto.getProduct() != null && pedto.getProduct().getId() != null) {
-                //     Product p = new Product();
-                //     p.setId(pedto.getProduct().getId());
-                //     pe.setProduct(p);
-                // }
-                return pe;
-            }).toList();
-            entity.setPurchaseEntries(purchaseEntries);
-        }
-
-        // if (dto.getPartyId() != null) {
-        //     entity.setParty(partyRepository.findById(dto.getPartyId())
-        //             .orElseThrow(() -> new ResourceNotFoundException("Party not found")));
-        // }
     }
 }
